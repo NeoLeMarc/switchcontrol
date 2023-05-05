09f563783423e06c100741c9d6b56c3efef7c5d3
Marcel Noe <marcel@marcel-noe.de>
2023-05-05 21:07:09 +0200

    found a good introduction on using snmp in python

---
+++ vlantest.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3  
+# -*- coding: utf-8 -*-
+
+# https://www.yaklin.ca/2021/08/25/snmp-queries-with-python.html
+
+
+# get the vlan information from the switch
+# and print it out in a nice format
+# use snmp to get vlans from switch
+import snmp_helper
