f5b157a80ff8eabf4aa978540aaf09b5325bac0b
Marcel Noe <marcel@marcel-noe.de>
2023-05-06 23:06:51 +0200

    In theory pysnmp works - however, Mikrotik does not implement all vlan funtions over SNMP. Next idea: Use RouterOS-Api

--- vlantest.py
+++ vlantest.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python3  
 # -*- coding: utf-8 -*-
+# There is currently a bug in working together with pyasn1 > 0.4.8
+# Hotfix: pip3 install pyasn1==0.4.8
 
 # https://www.yaklin.ca/2021/08/25/snmp-queries-with-python.html
 
+from pysnmp.entity.rfc3413.oneliner import cmdgen
+import sys
+host = 'ka-10ge-sw.wlan.ka.xcore.net'
 
-# get the vlan information from the switch
-# and print it out in a nice format
-# use snmp to get vlans from switch
-import snmp_helper
+SYSNAME = '1.3.6.1.2.1.1.5.0'
+snmp_ro_comm = 'public'
+
+# Define a PySNMP CommunityData object
+auth = cmdgen.CommunityData(snmp_ro_comm)
+
+# Define the CommandGenerator
+cmdGen = cmdgen.CommandGenerator()
+
+# Query a network device using the getCmd() function, providing the auth object,
+# a UDP transport, our OID for SYSNAME and don't lookup the OID in PySNMP's MIB's
+errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
+    auth,
+    cmdgen.UdpTransportTarget((host, 161)),
+    cmdgen.MibVariable(SYSNAME),
+    lookupNames=False)
+
+# Check wether there was an error querying the device
+if errorIndication:
+    sys.exit()
+
+for oid, val in varBinds:
+    print(oid.prettyPrint(), val.prettyPrint())
+
