8de38a963a0c8f7ab188b5b5472fb918a3880277
Marcel Noe <marcel@marcel-noe.de>
2023-05-07 21:52:47 +0200

    can now get vlans and pvids per port

--- mikrotik/vlan.py
+++ mikrotik/vlan.py
@@ -27,11 +27,30 @@ class VlanInfo(object):
         vlans = connection(cmd='/interface/bridge/vlan/print')
         return vlans
 
+    def getPvids(self, host):
+        # Get port information from switch
+        connection = librouteros.connect(
+            host=host,
+            username=self.config['auth']['username'],
+            password=self.config['auth']['password'],
+            use_ssl=True
+        )
+
+        ports = connection(cmd='/interface/bridge/port/print')
+
+        # Extract pvids from ports
+        pvids = {}
+        for port in ports:
+            pvids[port['interface']] = port['pvid']
+
+        return pvids
+
     def _buildVlanDB(self):
         # Get list of vlans from each switch
         for switch in self.switches['switches']:
             vlans = self._getVlans(switch['hostname'])
             switchVlans = [vlan for vlan in vlans]
+            pvids = self.getPvids(switch['hostname'])
 
             vlanTemp = {}
 
@@ -41,16 +60,46 @@ class VlanInfo(object):
                 currentTaggedPorts = vlan['current-tagged'].split(',')
                 currentUntaggedPorts = vlan['current-untagged'].split(',')
 
+                if len(currentTaggedPorts) > len(taggedPorts):
+                    print("Warning: Current tagged ports is greater than tagged ports for vlan " + str(vlan['vlan-ids']) + " on switch " + switch['name'])
+
+                if len(currentUntaggedPorts) > len(untaggedPorts):
+                    print("Warning: Current untagged ports is greater than untagged ports for vlan " + str(vlan['vlan-ids']) + " on switch " + switch['name'])
+
                 vlanTemp[vlan['vlan-ids']] = {
                     'tagged': taggedPorts,
                     'untagged': untaggedPorts,
                     'current-tagged': currentTaggedPorts,
                     'current-untagged': currentUntaggedPorts
+
                 }
 
-            self.vlanDB[switch['name']] = vlanTemp
+            self.vlanDB[switch['name']] = {'vlans' : vlanTemp, 'pvids': pvids}
 
     def getPortVlans(self, switchname, port):
         # Get list of vlans from switch
-        vlans = self.vlanDB[switchname]
-        print(vlans)
+        vlanDB = self.vlanDB[switchname]
+        vlans = vlanDB['vlans']
+        pvids = vlanDB['pvids']
+
+        # Find vlans that are tagged on port
+        taggedVlans = []
+        for vlan in vlans:
+            if port in vlans[vlan]['tagged']:
+                taggedVlans.append(vlan)
+
+        # Find vlans that are untagged on port
+        untaggedVlans = []
+        for vlan in vlans:
+            if port in vlans[vlan]['untagged']:
+                untaggedVlans.append(vlan)
+
+        if pvids[port] not in untaggedVlans:
+            print("Warning: PVID " + str(pvids[port]) + " is not in untagged vlans for port " + port + " on switch " + switchname)
+
+        return {
+            'tagged': taggedVlans,
+            'untagged': untaggedVlans,
+            'pvid' :pvids[port]
+        }
+
