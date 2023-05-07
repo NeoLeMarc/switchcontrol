35150d73a9232377744079216d43d0918cfaaa9f
Marcel Noe <marcel@marcel-noe.de>
2023-05-07 18:27:34 +0200

    changed format of switches.yaml - can now get list of vlans from each switch

--- mikrotik/vlantest.py
+++ mikrotik/vlantest.py
@@ -12,15 +12,30 @@ with open(path, 'r') as stream:
 
 # Use mikrotik API to connect to switch
 import librouteros
-connection = librouteros.connect(
-    host=config['switch']['host'],
-    username=config['auth']['username'],
-    password=config['auth']['password'],
-    use_ssl=True
-)
 
-# Get list of VLANs
-vlans = connection(cmd='/interface/bridge/vlan/print')
+def getVlans(switch):
+    connection = librouteros.connect(
+        host=config['switch']['host'],
+        username=config['auth']['username'],
+        password=config['auth']['password'],
+        use_ssl=True
+    )
 
-for vlan in vlans:
-    print(vlan)
+    # Get list of VLANs
+    vlans = connection(cmd='/interface/bridge/vlan/print')
+    return vlans
+
+# Get neighboring information from yaml
+with open('../switches.yaml', 'r') as stream:
+    switchconfig = yaml.safe_load(stream)
+    switches = switchconfig['switches']
+
+# Get list of vlans from each switch
+vlanDB = {}
+
+for switch in switches:
+    vlans = getVlans(switch['hostname'])
+    vlanDB[switch['name']] = [vlan for vlan in vlans]
+
+for switch in vlanDB:
+    print(vlanDB[switch])
