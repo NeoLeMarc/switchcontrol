8be389ec9ec723a94736e8663e69c2ddd683ccba
Marcel Noe <marcel@marcel-noe.de>
2023-12-05 00:07:53 +0100

    added hostname to commit message of backup script

--- backup-switches.sh
+++ backup-switches.sh
@@ -3,6 +3,6 @@ cd mikrotik
 python3 download-config.py all -w
 cd ~/switchcontrol-configs
 git add **/*.rsc
-git commit -m 'backup of config'
+git commit -m "backup of config - created from host $HOSTNAME"
 git pull
 git push origin
