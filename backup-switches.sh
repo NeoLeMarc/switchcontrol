9d78afa93347690321ddd553ff304d6425d81acd
Marcel Noe <marcel@marcel-noe.de>
2023-12-05 00:03:00 +0100

    changed git pull strategy to merge changes

--- backup-switches.sh
+++ backup-switches.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-cd ~/switchcontrol/mikrotik
+cd mikrotik
 python3 download-config.py all -w
 cd ~/switchcontrol-configs
 git add **/*.rsc
 git commit -m 'backup of config'
+git pull
 git push origin
